2d11b60b270b1c95765ba81bbf77d284ccb4a5d7
YJKBigBo <yjg0557@naver.com>
2024-11-25 12:25:33 +0900

    ✨feature : 프로젝트 생성 및 메인 화면

---
+++ shoppingB/src/test/java/mini/shoppingb/ShoppingBApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.shoppingb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
