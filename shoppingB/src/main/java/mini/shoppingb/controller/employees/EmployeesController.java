56113a62a12af4a23593e362df77fdfa32cb41fc
YJKBigBo <yjg0557@naver.com>
2024-11-27 09:58:35 +0900

    🎨Improve structure : 관리자 컨트롤러 구조 변경

--- shoppingB/src/main/java/mini/shoppingb/controller/employees/EmployeesController.java
+++ shoppingB/src/main/java/mini/shoppingb/controller/employees/EmployeesController.java
@@ -1,16 +1,40 @@
 package mini.shoppingb.controller.employees;
 
+import jakarta.servlet.http.HttpSession;
 import mini.shoppingb.command.employees.EmployeeCommand;
+import mini.shoppingb.command.employees.LoginCommand;
+import mini.shoppingb.service.employees.LoginService;
 import mini.shoppingb.service.employees.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RegisterController {
+public class EmployeesController {
+    @Autowired
+    LoginService loginService;
+
     @Autowired
     RegisterService registerService;
+
+    @GetMapping("/employees/login")
+    public String login() {
+        return "thymeleaf/employees/login";
+    }
+
+    @PostMapping("/employees/login")
+    public String loginPost(LoginCommand loginCommand, HttpSession session) {
+        loginService.execute(loginCommand, session);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/employees/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
 
     @GetMapping("/employees/register")
     public String registerPage() {
